70c0c61401aa11e82686fc2f789b84499b8839df
Yuta Fujii <yutayutakid21@gmail.com>
2020-07-01 10:11:04 +0900

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Yuta Fujii on 2020/07/01.
+//  Copyright © 2020 Yuta Fujii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
