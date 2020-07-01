0d63f5b1178136497775ceb17807a7d1860dff38
Yuta Fujii <yutayutakid21@gmail.com>
2020-07-01 10:14:13 +0900

    test

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("あいうえお")
+         print("かきくけこ")
+        
         // Do any additional setup after loading the view.
     }
 
